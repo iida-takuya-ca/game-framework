3b1124cc07335fc80038e2505474f2667c2d1055
ito_hironori <daitoku1988@gmail.com>
2023-01-12 05:52:00 +0900

    モデルビューア作成

--- Assets/SampleGame/Scripts/Viewer/ModelViewerManager.cs
+++ Assets/SampleGame/Scripts/Viewer/ModelViewerManager.cs
@@ -1,10 +1,93 @@
+using System;
+using GameFramework.BodySystems;
+using GameFramework.Core;
 using GameFramework.TaskSystems;
+using UnityEngine;
+using UniRx;
 
 namespace SampleGame {
     /// <summary>
     /// モデルビューア管理用クラス
     /// </summary>
-    public class ModelViewerManager : LateUpdatableTaskBehaviour
-    {
+    public class ModelViewerManager : LateUpdatableTaskBehaviour {
+        [SerializeField, Tooltip("ビューア用のデータ")]
+        private ModelViewerData _data;
+
+        // Body生成中のScope
+        private DisposableScope _bodyScope;
+
+        // 読み込み可能なBodyDataのIdリスト
+        public string[] BodyDataIds => _data.bodyDataIds;
+        // 現在使用中のBodyData
+        public ModelViewerBodyData CurrentBodyData { get; private set; }
+        // 現在使用中のBody
+        public Body CurrentBody { get; private set; }
+
+        /// <summary>
+        /// Bodyの生成
+        /// </summary>
+        public void SetupBody(string bodyDataId, Action onCreated) {
+            CleanupBody();
+
+            _bodyScope = new DisposableScope();
+
+            var bodyManager = Services.Get<BodyManager>();
+            
+            new ModelViewerBodyDataRequest(bodyDataId)
+                .LoadAsync(_bodyScope)
+                .Subscribe(data => {
+                    CurrentBodyData = data;
+                    // Bodyの生成
+                    CurrentBody = bodyManager.CreateFromPrefab(data.prefab);
+                    onCreated?.Invoke();
+                })
+                .ScopeTo(_bodyScope);
+        }
+
+        /// <summary>
+        /// 現在生成されているBodyの削除
+        /// </summary>
+        public void CleanupBody() {
+            if (CurrentBodyData == null) {
+                return;
+            }
+
+            if (CurrentBody != null) {
+                CurrentBody.Dispose();
+                CurrentBody = null;
+            }
+
+            if (_bodyScope != null) {
+                _bodyScope.Dispose();
+                _bodyScope = null;
+            }
+
+            CurrentBodyData = null;
+        }
+
+        /// <summary>
+        /// モーションの設定
+        /// </summary>
+        public void SetMotion(int index) {
+            if (CurrentBody == null) {
+                return;
+            }
+
+            if (index < 0 || index >= CurrentBodyData.animationClips.Length) {
+                return;
+            }
+
+            var clip = CurrentBodyData.animationClips[index];
+            if (clip == null) {
+                return;
+            }
+
+            var motionController = CurrentBody.GetController<MotionController>();
+            if (motionController == null) {
+                return;
+            }
+
+            motionController.Player.SetMotion(clip, 0.2f);
+        }
     }
 }
--- Assets/SampleGame/Scripts/Viewer/ModelViewerSceneSituation.cs
+++ Assets/SampleGame/Scripts/Viewer/ModelViewerSceneSituation.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using GameFramework.BodySystems;
 using GameFramework.Core;
 using GameFramework.SituationSystems;
 using UnityDebugSheet.Runtime.Core.Scripts;
@@ -12,16 +15,54 @@ namespace SampleGame {
         public override string SceneAssetPath => "model_viewer";
 
         /// <summary>
+        /// 初期化処理
+        /// </summary>
+        protected override IEnumerator SetupRoutineInternal(TransitionHandle handle, IScope scope) {
+            yield return base.SetupRoutineInternal(handle, scope);
+
+            var bodyManager = new BodyManager();
+            bodyManager.RegisterTask(TaskOrder.Body);
+            ServiceContainer.Set(bodyManager);
+        }
+
+        /// <summary>
         /// アクティブ時処理
         /// </summary>
         protected override void ActivateInternal(TransitionHandle handle, IScope scope) {
             base.ActivateInternal(handle, scope);
 
+            var viewerManager = Services.Get<ModelViewerManager>();
             var debugSheet = Services.Get<DebugSheet>();
+            
             var rootPage = debugSheet.GetOrCreateInitialPage();
-            _debugPageId = rootPage.AddPageLinkButton("Models", onLoad: page => {
-                page.AddButton("Test1", clicked: () => {
+            var motionsPageId = -1;
+            _debugPageId = rootPage.AddPageLinkButton("Model Viewer", onLoad: page => {
+                page.AddPageLinkButton("Models", onLoad: modelsPage => {
+                    var bodyDataIds = viewerManager.BodyDataIds;
+                    foreach (var bodyDataId in bodyDataIds) {
+                        var id = bodyDataId;
+                        modelsPage.AddButton(bodyDataId, clicked:() =>
+                        {
+                            viewerManager.CleanupBody();
+                            
+                            if (motionsPageId >= 0) {
+                                page.RemoveItem(motionsPageId);
+                            }
 
+                            viewerManager.SetupBody(id, () => {
+                                motionsPageId = page.AddPageLinkButton("Motions", onLoad: motionsPage => {
+                                    var clips = viewerManager.CurrentBodyData.animationClips;
+                                    for (var i = 0; i < clips.Length; i++) {
+                                        var clip = clips[i];
+                                        var index = i;
+                                        motionsPage.AddButton(clip.name, clicked: () => {
+                                            viewerManager.SetMotion(index);
+                                        });
+                                    }
+                                });
+                            });
+                        });
+                    }
                 });
             });
         }
