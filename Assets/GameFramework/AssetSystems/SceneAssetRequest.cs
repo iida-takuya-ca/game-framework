f79e16b5e995409297e3350c3667a25574ca4f2c
ito_hironori <daitoku1988@gmail.com>
2023-02-19 01:34:14 +0900

    Scene読み込み時の処理修正

--- Assets/GameFramework/AssetSystems/SceneAssetRequest.cs
+++ Assets/GameFramework/AssetSystems/SceneAssetRequest.cs
@@ -1,4 +1,5 @@
 using GameFramework.Core;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace GameFramework.AssetSystems {
@@ -40,6 +41,9 @@ namespace GameFramework.AssetSystems {
             if (handle.IsValid) {
                 // 解放処理を仕込む
                 unloadScope.OnExpired += () => handle.Release();
+            }
+            else {
+                Debug.LogError($"Not found provider. [{address}]");
             }
 
             return handle;
